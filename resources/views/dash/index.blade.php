87ac7daddce4b8a254d1cfd67458bfbc6817d0ad
Frankva12 <stanleyvasconcelos0@gmail.com>
2021-10-08 18:53:06 -0600

    Comanderia de dashboard y cambios de nombre

--- resources/views/dash/index.blade.php
+++ resources/views/dash/index.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Bienvenido a ARSS.</p>
 @stop
 
 @section('css')
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dash', function () {
+    return view('dash.index');
+})->name('dash');
