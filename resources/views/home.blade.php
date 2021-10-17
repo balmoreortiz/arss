08ace274aea7930dbcba2b72441091cc6d913bef
Svillatoro <91702978+Svillatoro@users.noreply.github.com>
2021-10-17 13:41:13 -0600

    Modificacion2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,64 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="text-center">Dashboard Content</h3>
+                            <div class="row">
+                                <div class="col-md-4 col-xl-4">
+
+                                    <div class="card bg-c-blue order-card">
+                                        <div class="card-block">
+                                        <h5>Usuarios</h5>                                               
+                                            @php
+                                             use App\Models\User;
+                                            $cant_usuarios = User::count();                                                
+                                            @endphp
+                                            <h2 class="text-right"><i class="fa fa-users f-left"></i><span>{{$cant_usuarios}}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más</a></p>
+                                        </div>                                            
+                                    </div>                                    
+                                </div>
+                                
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                        <h5>Roles</h5>                                               
+                                            @php
+                                            use Spatie\Permission\Models\Role;
+                                             $cant_roles = Role::count();                                                
+                                            @endphp
+                                            <h2 class="text-right"><i class="fa fa-user-lock f-left"></i><span>{{$cant_roles}}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/roles" class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
+                                </div>                                                                
+                                
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-pink order-card">
+                                        <div class="card-block">
+                                            <h5>Servicios</h5>                                               
+                                            @php
+                                             use App\Models\Servicio;
+                                            $cant_servicios = Servicio::count();                                                
+                                            @endphp
+                                            <h2 class="text-right"><i class="fa fa-servicio f-left"></i><span>{{$cant_servicios}}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/servicios" class="text-white">Ver más</a></p>
+                                            /div>
+                                        </div>
+                                    </div>                                                                
+                                    
+                                    <div class="col-md-4 col-xl-4">
+                                        <div class="card bg-c-pink order-card">
+                                            <div class="card-block">
+                                                <h5>Repuestos</h5>                                               
+                                                @php
+                                                 use App\Models\Repuesto;
+                                                $cant_servicios = Servicio::count();                                                
+                                                @endphp
+                                                <h2 class="text-right"><i class="fa fa-repuesto f-left"></i><span>{{$cant_repuestos}}</span></h2>
+                                                <p class="m-b-0 text-right"><a href="/repuestos" class="text-white">Ver más</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>      
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,18 +1,18 @@
 <li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
-    <a class="nav-link" href="/">
+    <a class="nav-link" href="home/">
         <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
     <a class="nav-link" href="/usuarios">
-        <i class=" fas fa-user"></i><span>Usuario</span>
+        <i class=" fas fa-user"></i><span>Usuarios</span>
     </a>
     <a class="nav-link" href="/roles">
         <i class=" fas fa-user-lock"></i><span>Roles</span>
     </a>
     <!--Por el momento se dejará / en esta vistas-->
-    <a class="nav-link" href="/">
-        <i class=" fas fa-truck"></i><span>Servicio</span>
+    <a class="nav-link" href="servicios/">
+        <i class=" fas fa-truck"></i><span>Servicios</span>
     </a>
-    <a class="nav-link" href="/">
-        <i class=" fas fa-wrench"></i><span>Repuesto</span>
+    <a class="nav-link" href="repuestos/">
+        <i class=" fas fa-wrench"></i><span>Repuestos</span>
     </a>
 </li>
