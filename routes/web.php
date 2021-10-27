d497652bbda80df415a3410ca32016333e9a17e0
Kevin Agreda <36802620+KevinAgreda245@users.noreply.github.com>
2021-10-27 08:32:31 -0600

    Buscador de servicios y repuestos

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolController;
 use App\Http\Controllers\ServicioController;
 use App\Http\Controllers\UsuarioController;
+use App\Models\Repuesto;
+use App\Models\Servicio;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,8 +21,13 @@ use App\Http\Controllers\UsuarioController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $nombre = $request->get('buscarpor');        
+
+    $repuestos = Repuesto::where('NOM_REP','like',"%$nombre%")->latest()->get();
+    $servicios = Servicio::where('NOM_SERV','like',"%$nombre%")->latest()->get();
+    
+    return view('welcome',compact('repuestos','servicios','nombre'));
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -32,5 +41,4 @@ Route::group(['middleware'=>['auth']],function(){
     Route::resource('usuarios', UsuarioController::class);
     Route::resource('repuestos', RepuestoController::class);
     Route::resource('servicios', ServicioController::class);
-
 });
