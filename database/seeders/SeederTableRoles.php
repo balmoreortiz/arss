e3ccd92bdc4312555690dbc8f3048253093c9f47
Frankva12 <stanleyvasconcelos0@gmail.com>
2021-11-16 18:51:38 -0600

    Cambios de funcionamiento

--- database/seeders/SeederTableRoles.php
+++ database/seeders/SeederTableRoles.php
@@ -23,6 +23,10 @@ class SeederTableRoles extends Seeder
         $id = 1;
         foreach($roles as $rol){
             $role = Role::create(['id' => $id , 'name'=>$rol]);
+            if($id==1)
+                $role->syncPermissions(['1','2', '3', '4','4','5','6','7','8','9','10','11','12','13','14','15','16']);
+            else
+                $role->syncPermissions(['9','13']);
             $id++;             
         }
     }
